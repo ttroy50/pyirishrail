83232a65a53317fbcc2a41938165912c51b23515
Thom Troy <ttroy50@gmail.com>
2017-10-15 13:51:49 +0100

    update to v0.0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='pyirishrail',
-      version='0.0.1',
+      version='0.0.2',
       description='Python library to get the real-time transport information (RTPI) from Irish Rail',
       keywords='irish rail RTPI',
       author='Thom Troy',
       author_email='ttroy50@gmail.com',
       license='MIT',
       url='https://github.com/ttroy50/pyirishrail',
-      download_url='https://github.com/ttroy50/pyirishrail/archive/0.0.1.tar.gz',
+      download_url='https://github.com/ttroy50/pyirishrail/archive/0.0.2.tar.gz',
       platforms=["any"],
       packages=find_packages(),
       zip_safe=False,
